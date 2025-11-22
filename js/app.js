4b5d898900642be0a5d94492fbb8d464cc3cda42
DanielDeSantos <danieldesantosgarres@gmail.com>
2025-11-23 00:06:47 +0100

    A

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log("app.js cargado");
+// Carga inicial del tablero
+document.addEventListener("DOMContentLoaded", () => {
+    const boardElement = document.getElementById("board");
+
+    // Crear instancia de Chessground
+    const ground = Chessground(boardElement, {
+        fen: "start",   // tablero inicial
+        viewOnly: false,
+        draggable: {
+            enabled: true
+        }
+    });
+
+    console.log("Chessground cargado");
+});
